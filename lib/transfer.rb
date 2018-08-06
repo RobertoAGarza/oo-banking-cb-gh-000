0af9ba4637baf3d3dfb87ca9ca8a2adf6bbd9033
Roberto Garza <robertg4567@hotmail.com>
2018-08-06 20:25:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   def execute_transaction
     if status == "pending"
       if sender.valid?
-        sender.balance -= amount
+        self.sender.balance -= amount
         receiver.balance+= amount
         status = "complete"
       else
